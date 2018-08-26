ca94aa9717f79316df9553d4211cec5c01297c7b
simpego81 <pegoraro.simone.1981@gmail.com>
2018-08-26 16:44:21 +0200

    Added Windows DLL project

---
+++ Org4n1zm/Org4n1zm/Org4n1zm.h
@@ -0,0 +1,22 @@
+// Il seguente blocco ifdef rappresenta il modo standard di creare macro che semplificano 
+// l'esportazione da una DLL. Tutti i file all'interno della DLL sono compilati con il simbolo ORG4N1ZM_EXPORTS
+// definito nella riga di comando. Questo simbolo non deve essere definito in alcun progetto
+// che utilizza questa DLL. In questo modo qualsiasi altro progetto i cui file di origine includono questo file vedranno le funzioni 
+// ORG4N1ZM_API come importate da una DLL, mentre la DLL vedrà i simboli
+// definiti con questa macro come esportati.
+#ifdef ORG4N1ZM_EXPORTS
+#define ORG4N1ZM_API __declspec(dllexport)
+#else
+#define ORG4N1ZM_API __declspec(dllimport)
+#endif
+
+// Questa classe è esportata da Org4n1zm.dll
+class ORG4N1ZM_API COrg4n1zm {
+public:
+	COrg4n1zm(void);
+	// TODO: aggiungere qui i metodi.
+};
+
+extern ORG4N1ZM_API int nOrg4n1zm;
+
+ORG4N1ZM_API int fnOrg4n1zm(void);
